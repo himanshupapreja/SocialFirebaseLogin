b6be6b08858f968be6630d9fba603d006f953943
HIMANSHU PAPREJA <idhi.nimansh@gmail.com>
2021-11-20 14:58:24 +0530

    Google Login code pushed

--- SocialFirebaseLogin.iOS/AppDelegate.cs
+++ SocialFirebaseLogin.iOS/AppDelegate.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-
 using Foundation;
+using Google.SignIn;
+using FirebaseCore = Firebase.Core;
+using Firebase.Auth;
 using UIKit;
+using System.Threading.Tasks;
 
 namespace SocialFirebaseLogin.iOS
 {
@@ -11,8 +14,10 @@ namespace SocialFirebaseLogin.iOS
     // User Interface of the application, as well as listening (and optionally responding) to 
     // application events from iOS.
     [Register("AppDelegate")]
-    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate, ISocialLogin
     {
+
+        static Action<SocialLoginUser, SocialLoginEnum, string> _onLoginComplete;
         //
         // This method is invoked when the application has loaded and is ready to run. In this 
         // method you should instantiate the window, load the UI into it and then make the window
@@ -23,9 +28,43 @@ namespace SocialFirebaseLogin.iOS
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
             global::Xamarin.Forms.Forms.Init();
+            FirebaseCore.App.Configure();
             LoadApplication(new App());
 
             return base.FinishedLaunching(app, options);
+        }
+
+        public override bool OpenUrl(UIApplication app, NSUrl url, NSDictionary options)
+        {
+            //return base.OpenUrl(app, url, options);
+            return SignIn.SharedInstance.HandleUrl(url);
+        }
+
+
+        public Task NativeSocialSignin(Action<SocialLoginUser, SocialLoginEnum, string> OnLoginComplete, SocialLoginEnum socialLoginType)
+        {
+            _onLoginComplete = OnLoginComplete;
+            if (socialLoginType.Equals(SocialLoginEnum.Google))
+            {
+                Google.SignIn.SignIn.SharedInstance.SignInUser();
+            }
+            else if (socialLoginType.Equals(SocialLoginEnum.Facebook))
+            {
+            }
+
+            return Task.CompletedTask;
+        }
+
+        public Task NativeSocialSignout(SocialLoginEnum socialLoginType)
+        {
+            if (socialLoginType.Equals(SocialLoginEnum.Google))
+            {
+            }
+            else if (socialLoginType.Equals(SocialLoginEnum.Facebook))
+            {
+            }
+
+            return Task.CompletedTask;
         }
     }
 }
---
+++ SocialFirebaseLogin.iOS/SocialLogin_iOS.cs
@@ -0,0 +1,40 @@
+﻿using Foundation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UIKit;
+
+namespace SocialFirebaseLogin.iOS
+{
+    public class SocialLogin_iOS : ISocialLogin
+    {
+        static Action<SocialLoginUser, SocialLoginEnum, string> _onLoginComplete;
+        public Task NativeSocialSignin(Action<SocialLoginUser, SocialLoginEnum, string> OnLoginComplete, SocialLoginEnum socialLoginType)
+        {
+            _onLoginComplete = OnLoginComplete;
+            if (socialLoginType.Equals(SocialLoginEnum.Google))
+            {
+                Google.SignIn.SignIn.SharedInstance.SignInUser();
+            }
+            else if (socialLoginType.Equals(SocialLoginEnum.Facebook))
+            {
+            }
+
+            return Task.CompletedTask;
+        }
+
+        public Task NativeSocialSignout(SocialLoginEnum socialLoginType)
+        {
+            if (socialLoginType.Equals(SocialLoginEnum.Google))
+            {
+            }
+            else if (socialLoginType.Equals(SocialLoginEnum.Facebook))
+            {
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
